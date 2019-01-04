ed730a6c4b8207821eff433515953b847f1f8c6a
Damien GAILLARD <damien.gaillard@norsys.fr>
2019-01-04 15:33:58 +0100

    feat: fabric

--- app/src/main/java/com/boarderscore/boarderscore/activities/HomeActivity.kt
+++ app/src/main/java/com/boarderscore/boarderscore/activities/HomeActivity.kt
@@ -6,14 +6,21 @@ import android.support.v7.app.AppCompatDelegate
 import com.boarderscore.boarderscore.R
 import com.boarderscore.boarderscore.fragments.HomeFragment
 import com.boarderscore.boarderscore.utils.SharedPref
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_home)
         AppCompatDelegate.setDefaultNightMode(
             AppCompatDelegate.MODE_NIGHT_AUTO
         )
         supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment.newInstance()).commit()
     }
+
+
 }
